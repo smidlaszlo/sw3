effa0e0ceb999b842b08126fbc89e02a0f18565a
smidlaszlo <smid@iit.uni-miskolc.hu>
2018-09-26 10:46:31 +0200

    auto kezdeti allapota

---
+++ 1_gyakorlat_auto/1_gyakorlat_auto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_gyakorlat_auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
